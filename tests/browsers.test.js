911bae6c8e0e57735897b587fc7f71180b19fd29
Pedro "Pépa" Santos <9425683+pepa667@users.noreply.github.com>
2026-05-20 18:03:37 -0300

    Initial commit

---
+++ tests/browsers.test.js
@@ -0,0 +1,11 @@
+import { test } from '@playwright/test';
+
+import _config from '../_config';
+
+const HOST = _config.server.host;
+const PORT = _config.server.port;
+
+test('Test browsers', async ({ page }) => {
+  await page.goto(`http://${HOST}:${PORT}`);
+  await page.pause();
+});
